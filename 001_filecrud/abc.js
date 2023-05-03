569b60d81b91a0ae055be229fdee9a78bcbfee3b
jigneshmd <121917872+jigneshmd@users.noreply.github.com>
2023-05-03 11:49:03 +0530

    done
    ok

---
+++ 001_filecrud/abc.js
@@ -0,0 +1,28 @@
+const { log } = require("console")
+const fs = require("fs")
+const getFileData = (data) => {
+    const mydata = JSON.stringify(data)
+   
+    fs.writeFile('test.json',mydata,(data)=>{
+        console.log('file write succesfully');
+ })
+}
+
+const viewFile = ()=> {
+
+    const alldata = getdata()
+    console.log(alldata);
+}
+
+const getdata = ()=>{
+    try {
+        const data = fs.readFileSync("test.json","utf-8")
+        const mydata = JSON.parse(data)
+        return mydata;
+        
+    } catch (error) {
+        return[];
+    }
+}
+
+module.exports = { getFileData,viewFile}
---
+++ 001_filecrud/app.js
@@ -0,0 +1,23 @@
+const yargs = require("yargs")
+const fs = require("fs")
+const file1 = require("./abc")
+yargs.command({
+    command : "add",
+    builder : {
+        name : {
+            type : String
+        },
+        email : {
+            type : String
+        }  
+    },
+    handler : function (argv){
+        const data = {
+            name : argv.name,
+            email : argv.email
+        }
+        console.log(data);
+        file1.getFileData(data)
+    }
+})
+yargs.argv
